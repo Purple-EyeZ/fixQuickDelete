1c4076020045cfe4dd601b24a3589de3791df834
Purple_Ξye™ <168314373+Purple-EyeZ@users.noreply.github.com>
2024-12-04 05:51:09 +0100

    skibidi

--- plugins/QuickDelete/src/index.ts
+++ plugins/QuickDelete/src/index.ts
@@ -2,12 +2,16 @@ import { findByProps } from "@vendetta/metro";
 
 export default {
     onLoad: () => {
-        const i18n = findByProps("Messages");
+        const intl = findByProps("intl");
+        const runtimeHashMessageKey = findByProps("runtimeHashMessageKey");
 
-        if (i18n) {
-            console.log("[Plugin] i18n.Messages détectés :", Object.keys(i18n.Messages));
+        console.log("[Plugin] intl :", intl);
+        console.log("[Plugin] runtimeHashMessageKey :", runtimeHashMessageKey);
+
+        if (!intl || !runtimeHashMessageKey) {
+            console.error("[Plugin] Les modules nécessaires (intl ou runtimeHashMessageKey) sont introuvables !");
         } else {
-            console.error("[Plugin] i18n.Messages est introuvable !");
+            console.log("[Plugin] Les modules nécessaires sont disponibles.");
         }
     },
     onUnload: () => {},
