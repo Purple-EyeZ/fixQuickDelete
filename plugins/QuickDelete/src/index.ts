9d15c21f9f0245c964dbe12c3c5e2f97d7c38d46
Purple_Ξye™ <168314373+Purple-EyeZ@users.noreply.github.com>
2024-12-04 02:26:34 +0100

    i18n logs

--- plugins/QuickDelete/src/index.ts
+++ plugins/QuickDelete/src/index.ts
@@ -1,36 +1,10 @@
-import { findByProps } from "@vendetta/metro";
-import { i18n } from "@vendetta/metro/common"; // Import des traductions
-import { instead } from "@vendetta/patcher";
-
-let unpatch;
+import { i18n } from "@vendetta/metro/common";
 
 export default {
     onLoad: () => {
-        const Popup = findByProps("show", "openLazy");
-        if (Popup) {
-            console.log("[Plugin] Popup trouvé :", Popup);
-
-            const deleteMessageTitle = i18n?.Messages?.DELETE_MESSAGE;
-            console.log("[Plugin] Traduction pour 'Delete Message' :", deleteMessageTitle);
-
-            unpatch = instead("show", Popup, (args, fn) => {
-                console.log("[Plugin] Popup.show intercepté avec les arguments :", args);
-
-                if (args?.[0]?.title === deleteMessageTitle) {
-                    console.log("[Plugin] Suppression de message détectée. Exécution de onConfirm.");
-                    args[0].onConfirm?.();
-                } else {
-                    console.log("[Plugin] Autre popup détecté. Exécution par défaut.");
-                    fn(...args);
-                }
-            });
-        } else {
-            console.error("[Plugin] Impossible de trouver 'Popup'.");
-        }
+        console.log("[Plugin] i18n.Messages :", i18n.Messages);
     },
-
     onUnload: () => {
-        console.log("[Plugin] Déchargement...");
-        unpatch?.();
+        console.log("[Plugin] Déchargé.");
     },
 };
