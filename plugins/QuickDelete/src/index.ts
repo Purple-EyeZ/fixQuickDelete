e7770e0486ce48eb73fc9926b9aee33c9fbca25c
Purple_Ξye™ <168314373+Purple-EyeZ@users.noreply.github.com>
2024-12-04 06:08:05 +0100

    zzzzzzz

--- plugins/QuickDelete/src/index.ts
+++ plugins/QuickDelete/src/index.ts
@@ -1,25 +1,18 @@
-import { findByProps } from "@vendetta/metro";
+import { findByProps, findByName } from "@vendetta/metro";
 
 export default {
     onLoad: () => {
-        const intl = findByProps("intl");
-        const runtimeHashMessageKey = findByProps("runtimeHashMessageKey");
-
-        if (!intl || !runtimeHashMessageKey) {
-            console.error("[Plugin] Les modules nécessaires sont introuvables !");
-            return;
-        }
-
-        const testKey = "Delete Message";
-        const hash = runtimeHashMessageKey(testKey);
-
-        console.log(`[Plugin] Hash pour '${testKey}' :`, hash);
-
         try {
-            const message = intl.string(intl.t[hash]);
-            console.log(`[Plugin] Message récupéré pour '${testKey}' :`, message);
+            const intl = findByProps("intl");
+            console.log("[Plugin] intl contenu :", intl);
+
+            const messages = findByProps("Messages");
+            console.log("[Plugin] Messages disponibles :", Object.keys(messages || {}));
+
+            const allProps = findByProps("format", "locale");
+            console.log("[Plugin] Propriétés générales liées à la langue :", allProps);
         } catch (e) {
-            console.error("[Plugin] Erreur lors de la récupération du message :", e);
+            console.error("[Plugin] Une erreur est survenue :", e);
         }
     },
     onUnload: () => {},
