a80f626c199919c2170a4742fb19b7680e292a1b
Purple_Ξye™ <168314373+Purple-EyeZ@users.noreply.github.com>
2024-12-04 09:07:26 +0100

    efefef

--- plugins/RemoveEmbed/src/index.ts
+++ plugins/RemoveEmbed/src/index.ts
@@ -1,9 +1,9 @@
 import intlProxy from "../intlProxy";
 
-console.log("intlProxy.REMOVE_EMBED:", intlProxy.REMOVE_EMBED);
+console.log("intlProxy.DELETE_EMBED:", intlProxy.DELETE_EMBED);
 
-if (intlProxy.REMOVE_EMBED) {
-    console.log("intlProxy fonctionne : REMOVE_EMBED =", intlProxy.REMOVE_EMBED);
+if (intlProxy.DELETE_EMBED) {
+    console.log("intlProxy fonctionne : DELETE_EMBED =", intlProxy.DELETE_EMBED);
 } else {
-    console.warn("intlProxy ne fonctionne pas : REMOVE_EMBED est undefined ou null");
+    console.warn("intlProxy ne fonctionne pas : DELETE_EMBED est undefined ou null");
 }
